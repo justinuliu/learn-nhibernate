cf5fdf3b35ae11557ec5da66d87227186723f6d7
liujia <liujia7056@outlook.com>
2016-03-03 13:51:32 +0800

    first commit.

---
+++ FirstSolution/Repositories/NHibernateHelper.cs
@@ -0,0 +1,32 @@
+﻿using FirstSolution.Domain;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace FirstSolution.Repositories
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessiongFactory
+        {
+            get
+            {
+                if(_sessionFactory == null)
+                {
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    configuration.AddAssembly(typeof(Product).Assembly);
+                    _sessionFactory = configuration.BuildSessionFactory();
+                }
+                return _sessionFactory;
+
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessiongFactory.OpenSession();
+        }
+    }
+}
---
+++ FirstSolution/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using FirstSolution.Domain;
+using NHibernate;
+
+namespace FirstSolution.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public void Add(Product product)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Save(product);
+                transaction.Commit();
+            }
+        }
+
+        public ICollection<Product> GetByCategory(string category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(Guid productId)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            {
+                return session.Get<Product>(productId);
+            }
+        }
+
+        public Product GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Product product)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Delete(product);
+                transaction.Commit();
+            }
+        }
+
+        public void Update(Product product)
+        {
+            using (ISession session = NHibernateHelper.OpenSession())
+            using (ITransaction transaction = session.BeginTransaction())
+            {
+                session.Update(product);
+                transaction.Commit();
+            }
+        }
+
+    }
+}
